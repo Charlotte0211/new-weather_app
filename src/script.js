e4ac481eea41ee792927cac800f8e6263441b0b8
Charlotte0211 <69457852+Charlotte0211@users.noreply.github.com>
2024-08-01 07:13:25 +0100

    Add complete JS weather forecast

--- src/script.js
+++ src/script.js
@@ -8,12 +8,15 @@ function displayTemperature(response) {
   let wind = document.querySelector("#wind");
   let currentWind = response.data.wind.speed;
   let icon = document.querySelector("#weather-app-icon");
+
   wind.innerHTML = Math.round(currentWind);
   humidity.innerHTML = Math.round(currentHumidity);
   weatherDescription.innerHTML = response.data.condition.description;
   city.innerHTML = response.data.city;
   temperature.innerHTML = Math.round(currentTemperature);
   icon.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon">`;
+
+  getForecast(response.data.city);
 }
 
 function dateFormat(date) {
@@ -62,7 +65,7 @@ function dateFormat(date) {
 }
 
 function searchCity(city) {
-  let apiKey = "o4045te388f5bc6e0abcc5fba3a40236";
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
@@ -70,30 +73,48 @@ function searchCity(city) {
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
+
   searchCity(searchInput.value);
 }
 
-function displayForecast() {
-  let forecastElement = document.querySelector("#forecast");
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-  let days = ["Tue", "Wed", "Thus", "Fri", "Sat"];
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
 <div class="weather-forecast-day">
-            <div class="weather-forecast-date">${day}</div>
-            <div class="weather-forecast-icon">🌤️</div>
+            <div class="weather-forecast-date">${formatDay(day.time)}</div>
+
+            <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
             <div class="weather-forecast-temperatures">
               <div class="weather-forecast-temperature">
-                <strong>15º</strong>
+                <strong>${Math.round(day.temperature.maximum)}º</strong>
               </div>
-              <div class="weather-forecast-temperature">9º</div>
+              <div class="weather-forecast-temperature">${Math.round(
+                day.temperature.minimum
+              )}º
+              </div>
             </div>
           </div>
 `;
+    }
   });
 
   forecastElement.innerHTML = forecastHtml;
@@ -107,4 +128,3 @@ let date = new Date();
 currentDate.innerHTML = dateFormat(date);
 
 searchCity("Madrid");
-displayForecast();
